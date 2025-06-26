8814367f773a9cc4750d0a4dc9dcff84d3800ce4
LovelyCat <1252251361@qq.com>
2025-06-26 20:22:40 +0800

    doc(reflect): function descriptions

--- reflect/src/main/kotlin/com/lovelycatv/vertex/reflect/ReflectUtils.kt
+++ reflect/src/main/kotlin/com/lovelycatv/vertex/reflect/ReflectUtils.kt
@@ -29,6 +29,12 @@ object ReflectUtils {
             BaseDataType.ALL
     }
 
+    /**
+     * Get all declared fields (including parent's)
+     *
+     * @param target Object
+     * @return All declared field
+     */
     @JvmStatic
     fun getAllDeclaredFields(target: Any): List<Field> {
         val result = mutableListOf<Field>()
@@ -40,6 +46,14 @@ object ReflectUtils {
         return result
     }
 
+    /**
+     * Get all declared properties (including parent's)
+     *
+     * See also: [getAllDeclaredFields]
+     *
+     * @param target Object
+     * @return All declared properties.
+     */
     @JvmStatic
     @Suppress("UNCHECKED_CAST")
     fun getAllDeclaredProperties(target: Any): List<KProperty1<Any, *>> {
@@ -52,6 +66,15 @@ object ReflectUtils {
         return result
     }
 
+    /**
+     * Copy an object.
+     *
+     * @param T Type of the object to be copied.
+     * @param target Object to be copied.
+     * @param fieldValueTransformer After a new instance of the object created,
+     *                              determine how to assign field value to this new instance.
+     * @return New instance of the object.
+     */
     @JvmStatic
     @Throws(exceptionClasses = [NullPointerException::class])
     fun <T: Any> copyObject(target: T, fieldValueTransformer: (Any?) -> Any?): T {
