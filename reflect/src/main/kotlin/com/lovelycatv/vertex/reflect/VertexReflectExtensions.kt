5af950b077f89ec21622bbcd8dcdb6879752fa1e
LovelyCat <1252251361@qq.com>
2025-08-20 15:40:38 +0800

    feat(reflect): Class<*>.noArgsConstructor() extension

--- reflect/src/main/kotlin/com/lovelycatv/vertex/reflect/VertexReflectExtensions.kt
+++ reflect/src/main/kotlin/com/lovelycatv/vertex/reflect/VertexReflectExtensions.kt
@@ -1,5 +1,7 @@
 package com.lovelycatv.vertex.reflect
 
+import java.lang.reflect.Constructor
+
 /**
  * @author lovelycat
  * @since 2025-06-26 19:06
@@ -10,3 +12,19 @@ class VertexReflectExtensions private constructor()
 fun <T: Any> T.shallowCopy() = ReflectUtils.shallowCopy(this)
 
 fun <T: Any> T.deepCopy() = ReflectUtils.deepCopy(this)
+
+fun <T> Class<T>.noArgsConstructor(): Constructor<T>? {
+    return try {
+        this.getConstructor()
+    } catch (_: Exception) {
+        null
+    }
+}
+
+fun Array<out Constructor<*>>.noArgsConstructor(): Constructor<*>? {
+    return this.find { it.parameterCount == 0 }
+}
+
+fun Collection<Constructor<*>>.noArgsConstructor(): Constructor<*>? {
+    return this.find { it.parameterCount == 0 }
+}
